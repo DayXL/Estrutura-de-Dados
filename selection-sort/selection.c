5be7869099916dce828a4fd3514eccd6a329c816
DayXL <day.luc.xav0606@gmail.com>
2023-05-20 17:44:43 -0300

    selection sort

---
+++ selection-sort/selection.c
@@ -0,0 +1,52 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void selection_sort(int *v, unsigned int n) {
+    unsigned int i, j;
+    int m;
+    int aux;
+
+    for (i = 0; i < (n - 1); i++) {
+        m = i;
+
+        for (j = (i + 1); j < n; j++) {
+            if (v[m] > v[j]) {
+                m = j;
+            }
+                
+        }
+        
+        aux = v[m];
+        v[m] = v[i];
+        v[i] = aux;
+
+    }
+}
+
+int main(int argc, char **argv) {
+    struct timespec a, b;
+    unsigned int t, n;
+    int i, *v;
+
+    n = atoi(argv[1]);
+    v = (int *) malloc(n * sizeof(int));
+    srand(time(NULL));
+    for (i = 0; i < n; i++) {
+        v[i] = rand() % 1000;
+    }
+
+        
+    clock_gettime(CLOCK_MONOTONIC, &b);
+    selection_sort(v, n);
+    clock_gettime(CLOCK_MONOTONIC, &a);
+
+    t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
+
+    printf("%u\n", t);
+
+    free(v);
+
+    return 0;
+}
